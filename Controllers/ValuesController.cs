85a11950847373c15ff721c2a7324e929e4a3cce
rseroter <rseroter@hotmail.com>
2018-06-06 11:21:38 +0100

    Removed unused operations

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -34,23 +34,5 @@ namespace tested_core_app.Controllers
                 return "private";
             }
         }
-
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
     }
 }
